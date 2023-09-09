e73d7954ba50d5b2738185b62d38918e6883f8c0
AK-PK <129682500+AK-PK22@users.noreply.github.com>
2023-09-09 18:20:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 import tkinter as tk
+from tkinter import ttk
 import socket
 import threading
-
-def open_tools_page():
-    for widget in main_frame.winfo_children():
-        widget.destroy()
-    tools_label = tk.Label(main_frame, text="Tools", font=("Helvetica", 24))
-    tools_label.grid(row=0, column=0, pady=20)
-    back_button = tk.Button(main_frame, text="Zurück", command=main_frame)
-    back_button.grid(row=1, column=0, pady=20)
 
 def cancel_scan():
     global scan_cancelled
@@ -18,6 +11,7 @@ def cancel_scan():
 def scan_ports():
     target = entry.get()
     result_text.delete("1.0", tk.END)
+    port_range = port_entry.get().split('-')
 
     try:
         target_ip = socket.gethostbyname(target)
@@ -30,34 +24,57 @@ def scan_ports():
         sock.settimeout(1)
         result = sock.connect_ex((target_ip, port))
         if result == 0:
-            result_text.insert(tk.END, f"Port {port} ist offen.\n")
+            result_text.insert(tk.END, f"(Port {port} ist offen.)\n", "open")
+        else:
+            result_text.insert(tk.END, f"(Port {port} ist geschlossen.)\n", "closed")
         sock.close()
 
-    for port in range(1, 1025):
+    start_port = int(port_range[0])
+    end_port = int(port_range[1]) if len(port_range) > 1 else start_port
+
+    for port in range(start_port, end_port + 1):
         thread = threading.Thread(target=do_scan, args=(port,))
         thread.start()
+
+def show_help():
+    help_text.set("PortGhost ist ein einfaches Tool zum Scannen von Ports auf einem \nZielrechner.\n "
+                  "Du kannst einen Hostnamen oder eine IP-Adresse eingeben und einen \nPortbereich definieren.\n "
+                  "PortGhost wird dann versuchen, die angegebenen Ports auf Erreichbarkeit \nzu überprüfen und\n "
+                  "dir mitteilen, welche Ports offen oder geschlossen sind. Du kannst die \nErgebnisse in Echtzeit \nauf dem Bildschirm sehen.\n")
+
+def on_tab_change(event):
+    current_tab = notebook.index(notebook.select())
+    if current_tab == 1:
+        show_help()
 
 root = tk.Tk()
 root.title("PortGhost")
 root.geometry("800x600")
 
-main_frame = tk.Frame(root)
-main_frame.pack(side="left", fill="both", expand=True)
+notebook = ttk.Notebook(root)
+notebook.pack(fill="both", expand=True)
 
-background_color1 = (100, 100, 255)
-background_color2 = (255, 100, 100)
+main_frame = ttk.Frame(notebook)
+help_frame = ttk.Frame(notebook)
 
-tools_button = tk.Button(main_frame, text="Tools", bg="red", command=open_tools_page)
-tools_button.pack()
+notebook.add(main_frame, text="Hauptseite")
+notebook.add(help_frame, text="Hilfe")
 
-button = tk.Button(main_frame, text="Help", bg="red")
-button.pack(side="left")
+style = ttk.Style()
+style.configure("open.TLabel", foreground="green")
+style.configure("closed.TLabel", foreground="red")
 
 label = tk.Label(main_frame, text="PortGhost", font=("Helvetica", 24))
 label.pack(pady=20)
 
 entry = tk.Entry(main_frame, font=("Helvetica", 14))
 entry.pack(padx=20, pady=10)
+
+port_label = tk.Label(main_frame, text="Portbereich (z.B. 80-100):", font=("Helvetica", 14))
+port_label.pack(padx=20, pady=10)
+
+port_entry = tk.Entry(main_frame, font=("Helvetica", 14))
+port_entry.pack(padx=20, pady=10)
 
 scan_button = tk.Button(main_frame, text="Scan starten", command=scan_ports)
 scan_button.pack()
@@ -67,5 +84,14 @@ cancel_button.pack()
 
 result_text = tk.Text(main_frame, font=("Helvetica", 14))
 result_text.pack(padx=20, pady=20, fill='both', expand=True)
+
+result_text.tag_configure("open", foreground="green")
+result_text.tag_configure("closed", foreground="red")
+
+help_text = tk.StringVar()
+help_label = tk.Label(help_frame, textvariable=help_text, font=("Helvetica", 14))
+help_label.pack()
+
+notebook.bind("<<NotebookTabChanged>>", on_tab_change)
 
 root.mainloop()
