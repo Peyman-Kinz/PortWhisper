aa88de9341484ce1ebcf4c1173a88f9e9837117e
AK-PK <129682500+AK-PK22@users.noreply.github.com>
2023-09-10 10:34:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,25 +19,46 @@ def scan_ports():
         result_text.insert(tk.END, "Ungültiger Hostname")
         return
 
-    def do_scan(port):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)
-        result = sock.connect_ex((target_ip, port))
-        if result == 0:
-            result_text.insert(tk.END, f"(Port {port} ist offen.)\n", "open")
-        else:
-            result_text.insert(tk.END, f"(Port {port} ist geschlossen.)\n", "closed")
-        sock.close()
+    def do_scan(port, scan_type):
+        try:
+            if scan_type == "TCP":
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.settimeout(1)
+                result = sock.connect_ex((target_ip, port))
+            elif scan_type == "UDP":
+                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                result = sock.connect_ex((target_ip, port))
+            else:
+                result_text.insert(tk.END, "Ungültiger Scan-Typ")
+                return
+
+            if result == 0:
+                result_text.insert(tk.END, f"(Port {port} ist offen.)\n", "open")
+            else:
+                result_text.insert(tk.END, f"(Port {port} ist geschlossen.)\n", "closed")
+            sock.close()
+        except socket.timeout:
+            result_text.insert(tk.END, f"(Port {port} hat nicht geantwortet.)\n", "timeout")
 
     start_port = int(port_range[0])
     end_port = int(port_range[1]) if len(port_range) > 1 else start_port
 
+    selected_scan = scan_type_var.get()
+    scan_name = scan_types[selected_scan]
+
     for port in range(start_port, end_port + 1):
-        thread = threading.Thread(target=do_scan, args=(port,))
+        thread = threading.Thread(target=do_scan, args=(port, scan_name))
         thread.start()
 
 def show_help():
-    help_text.set("")
+    help_text.set("PortGhost ist ein einfaches Tool zum Scannen von Ports auf einem \n"
+                  "Zielrechner.\n "
+                  "Du kannst einen Hostnamen oder eine IP-Adresse eingeben und einen \n"
+                  "Portbereich definieren. Wähle den Scan-Typ aus und klicke auf 'Scan starten'.\n"
+                  "PortGhost wird dann versuchen, die angegebenen Ports auf Erreichbarkeit \n"
+                  "zu überprüfen und\n "
+                  "dir mitteilen, welche Ports offen, geschlossen oder nicht antwortend sind. Du kannst die \n"
+                  "Ergebnisse in Echtzeit \nauf dem Bildschirm sehen.\n")
 
 def on_tab_change(event):
     current_tab = notebook.index(notebook.select())
@@ -47,21 +68,20 @@ def on_tab_change(event):
 def enable_dark_mode():
     style = ttk.Style()
     style.configure("TFrame", background="grey")
-    style.configure("TEntry", foreground="black")
-    style.configure("TText", foreground="black")
-    style.configure("TEntry", fieldbackground="grey")
-    style.configure("TText", fieldbackground="grey")
+    style.configure("TEntry", foreground="black", fieldbackground="grey", bordercolor="grey")
+    style.configure("TText", foreground="black", fieldbackground="grey", bordercolor="grey")
     style.configure("TButton", background="grey", foreground="black")
     dark_button.configure(state=tk.DISABLED)
     white_button.configure(state=tk.NORMAL)
+    scan_button.configure(bg="blue", fg="white")
 
 def enable_white_mode():
     main_frame_canvas.configure(bg="white")
     help_frame_canvas.configure(bg="white")
     settings_frame_canvas.configure(bg="white")
     about_frame_canvas.configure(bg="white")
-    entry.configure(bg="white", fg="black")
-    port_entry.configure(bg="white", fg="black")
+    entry.configure(bg="white", fg="black", highlightbackground="white")
+    port_entry.configure(bg="white", fg="black", highlightbackground="white")
     result_text.configure(bg="white", fg="black")
     scan_button.configure(bg="white", fg="black")
     cancel_button.configure(bg="white", fg="black")
@@ -69,16 +89,14 @@ def enable_white_mode():
     white_button.configure(state=tk.DISABLED)
     style = ttk.Style()
     style.configure("TFrame", background="white")
-    style.configure("TEntry", foreground="black")
-    style.configure("TText", foreground="black")
-    style.configure("TEntry", fieldbackground="white")
-    style.configure("TText", fieldbackground="white")
+    style.configure("TEntry", foreground="black", fieldbackground="white", bordercolor="white")
+    style.configure("TText", foreground="black", fieldbackground="white", bordercolor="white")
     style.configure("TButton", background="white", foreground="black")
 
 root = tk.Tk()
 root.title("PortGhost")
 root.geometry("800x600")
-root.configure(bg="white")  # Hinzugefügt, um die Hintergrundfarbe auf Weiß zu setzen
+root.configure(bg="white")
 
 notebook = ttk.Notebook(root)
 notebook.pack(fill="both", expand=True)
@@ -118,6 +136,7 @@ result_text.pack(padx=20, pady=20, fill='both', expand=True)
 
 result_text.tag_configure("open", foreground="green")
 result_text.tag_configure("closed", foreground="red")
+result_text.tag_configure("timeout", foreground="orange")
 
 help_text = tk.StringVar()
 help_label = tk.Label(help_frame, textvariable=help_text, font=("Helvetica", 14))
@@ -135,7 +154,20 @@ about_frame_canvas.pack(fill="both", expand=True)
 settings_label = tk.Label(settings_frame, text="Einstellungen", font=("Helvetica", 24))
 settings_label.pack(pady=20)
 
-dark_button = tk.Button(settings_frame, text="Grey Modus", command=enable_dark_mode)
+scan_type_var = tk.IntVar()
+scan_type_var.set(0)  # Default: TCP-Scan
+scan_types = {0: "TCP", 1: "UDP"}
+
+scan_type_label = tk.Label(settings_frame, text="Scan-Typ:", font=("Helvetica", 14))
+scan_type_label.pack(padx=20, pady=10)
+
+tcp_scan_radio = tk.Radiobutton(settings_frame, text="TCP", variable=scan_type_var, value=0)
+udp_scan_radio = tk.Radiobutton(settings_frame, text="UDP", variable=scan_type_var, value=1)
+
+tcp_scan_radio.pack(padx=20, pady=5, anchor="w")
+udp_scan_radio.pack(padx=20, pady=5, anchor="w")
+
+dark_button = tk.Button(settings_frame, text="Dark Modus", command=enable_dark_mode)
 dark_button.pack(pady=10)
 
 white_button = tk.Button(settings_frame, text="White Modus", command=enable_white_mode)
